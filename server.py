a9b6176263a7b4471e62f3271e11b2d37f910c67
izasoerya <ihza.surya@gmail.com>
2025-07-16 21:57:51 +0700

    chore: change to localhosot

--- server.py
+++ server.py
@@ -50,4 +50,4 @@ def subscribe(client, mid, qos, properties):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
